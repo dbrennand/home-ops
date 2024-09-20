78a3ce15a409363bff28ff93bfb0b83a0bf4a239
dbrennand <52419383+dbrennand@users.noreply.github.com>
2024-09-20 17:38:15 +0100

    refactor: use `tflint-ignore`

--- terraform/op.tf
+++ terraform/op.tf
@@ -9,10 +9,11 @@ data "onepassword_item" "proxmox_virtual_environment" {
   title = var.op_proxmox_virtual_environment_item_name
 }
 
-#data "onepassword_item" "op_ssh_key" {
-#  vault = var.op_ssh_vault_name
-#  title = var.op_ssh_key_name
-#}
+# tflint-ignore: terraform_unused_declarations
+data "onepassword_item" "op_ssh_key" {
+  vault = var.op_ssh_vault_name
+  title = var.op_ssh_key_name
+}
 
 data "onepassword_item" "pihole" {
   vault = var.op_pihole_vault_name
