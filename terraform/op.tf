c65ff96900cf7e90d11b665438914803e4c5efe2
dbrennand <52419383+dbrennand@users.noreply.github.com>
2024-09-20 17:35:04 +0100

    refactor: comment out

--- terraform/op.tf
+++ terraform/op.tf
@@ -9,10 +9,10 @@ data "onepassword_item" "proxmox_virtual_environment" {
   title = var.op_proxmox_virtual_environment_item_name
 }
 
-data "onepassword_item" "op_ssh_key" {
-  vault = var.op_ssh_vault_name
-  title = var.op_ssh_key_name
-}
+#data "onepassword_item" "op_ssh_key" {
+#  vault = var.op_ssh_vault_name
+#  title = var.op_ssh_key_name
+#}
 
 data "onepassword_item" "pihole" {
   vault = var.op_pihole_vault_name
