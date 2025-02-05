513c266500ae73f33cf90625e0427529f18e2353
dbrennand <52419383+dbrennand@users.noreply.github.com>
2025-02-05 21:47:01 +0000

    [skip ci]chore: change region

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,19 +14,16 @@ terraform {
       version = "0.2.0"
     }
   }
-  # Unable to pass variables to backend config
-  # Tracked in: https://github.com/hashicorp/terraform/issues/13022
-  # OpenTofu issue: https://github.com/opentofu/opentofu/issues/1042
   backend "s3" {
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_s3_checksum            = true
     encrypt                     = true
     key                         = "terraform.tfstate"
-    region                      = "us-west-000"
+    region                      = "eu-central-003"
     bucket                      = "homeops"
     endpoints = {
-      s3 = "https://s3.us-west-000.backblazeb2.com"
+      s3 = "s3.eu-central-003.backblazeb2.com"
     }
   }
 }
