0a358feb9819fd1863896ac911708f2b98963427
dbrennand <52419383+dbrennand@users.noreply.github.com>
2025-02-25 22:46:37 +0000

    feat: `pihole02`

--- terraform/proxmox.tf
+++ terraform/proxmox.tf
@@ -5,10 +5,24 @@ provider "proxmox" {
   password = data.onepassword_item.proxmox_virtual_environment.password
 }
 
+module "proxmox_vm_pihole02" {
+  source                                           = "./modules/proxmox_vm"
+  proxmox_vm_name                                  = "pihole02"
+  proxmox_vm_id                                    = 100
+  proxmox_vm_tags                                  = ["vm", "opentofu"]
+  proxmox_vm_cores                                 = 2
+  proxmox_vm_memory                                = 2048
+  proxmox_vm_ip                                    = "192.168.0.5/24"
+  proxmox_vm_virtual_environment_disk_datastore_id = "lv-ssd-crucial"
+  proxmox_vm_virtual_environment_node_name         = "proxmox01"
+  proxmox_vm_cloud_init_config_ssh_authorized_keys = data.onepassword_item.ssh_key.public_key
+  proxmox_vm_disk_size                             = 50
+}
+
 module "proxmox_vm_exit01" {
   source                                           = "./modules/proxmox_vm"
   proxmox_vm_name                                  = "exit01"
-  proxmox_vm_id                                    = 100
+  proxmox_vm_id                                    = 101
   proxmox_vm_tags                                  = ["vm", "opentofu", "tailscale"]
   proxmox_vm_cores                                 = 1
   proxmox_vm_memory                                = 1024
