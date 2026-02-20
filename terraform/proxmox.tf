74a6ffb5b6097e9b4ac34cc86560676db0ba4a78
dbrennand <52419383+dbrennand@users.noreply.github.com>
2026-02-20 21:37:33 +0000

    feat: re-deploy `k3s01`

--- terraform/proxmox.tf
+++ terraform/proxmox.tf
@@ -18,9 +18,66 @@ resource "proxmox_virtual_environment_download_file" "almalinux_9_latest_cloud_i
   checksum_algorithm = "sha256"
 }
 
-module "proxmox_cloud_init_config_idp01" {
+module "proxmox_cloud_init_config_k3s01" {
   source                                                  = "./modules/proxmox_cloud_init_config"
-  proxmox_cloud_init_config_vm_name                       = "idp01"
+  proxmox_cloud_init_config_vm_name                       = "k3s01"
   proxmox_cloud_init_config_virtual_environment_node_name = "proxmox01"
   proxmox_cloud_init_config_ssh_authorized_keys           = data.onepassword_item.ssh_key.public_key
+}
+
+resource "proxmox_virtual_environment_vm" "k3s01" {
+  name        = "k3s01"
+  vm_id       = 104
+  description = "Created with OpenTofu."
+  tags        = ["vm", "opentofu", "tailscale", "192.168.0.8"]
+  node_name   = "proxmox01"
+  on_boot     = false
+  started     = true
+
+  agent {
+    enabled = true
+    trim    = true
+  }
+
+  cpu {
+    cores = 2
+    type  = "host"
+  }
+
+  scsi_hardware = "virtio-scsi-single"
+
+  disk {
+    interface    = "scsi0"
+    datastore_id = "lv-ssd-crucial"
+    file_id      = proxmox_virtual_environment_download_file.almalinux_9_latest_cloud_image.id
+    size         = 100
+    discard      = "on"
+    ssd          = true
+    iothread     = true
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  initialization {
+    datastore_id = "lv-ssd-crucial"
+    ip_config {
+      ipv4 {
+        address = "192.168.0.8/24"
+        gateway = "192.168.0.1"
+      }
+    }
+    user_data_file_id = module.proxmox_cloud_init_config_k3s01.cloud_init_config_id
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  network_device {
+    bridge   = "vmbr0"
+    enabled  = true
+    firewall = true
+  }
 }
