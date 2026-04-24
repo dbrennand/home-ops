237936f35cf1d766a897e41ddd8e4a9d546cd91d
dbrennand <52419383+dbrennand@users.noreply.github.com>
2026-04-24 08:17:51 +0100

    feat: `hermes01`

--- terraform/proxmox.tf
+++ terraform/proxmox.tf
@@ -14,7 +14,7 @@ resource "proxmox_virtual_environment_download_file" "almalinux_9_latest_cloud_i
   node_name          = "proxmox01"
   file_name          = "AlmaLinux-9-GenericCloud-latest.x86_64.qcow2.img"
   url                = "https://repo.almalinux.org/almalinux/9/cloud/x86_64/images/AlmaLinux-9-GenericCloud-latest.x86_64.qcow2"
-  checksum           = "5ff9c048859046f41db4a33b1f1a96675711288078aac66b47d0be023af270d1"
+  checksum           = "0a07f59b59efba1bbdbe07e69c11e291f015e602ed614596f064ebe00c1bdf9b"
   checksum_algorithm = "sha256"
 }
 
@@ -32,7 +32,7 @@ resource "proxmox_virtual_environment_vm" "k3s01" {
   tags        = ["vm", "opentofu", "tailscale", "192.168.0.8"]
   node_name   = "proxmox01"
   on_boot     = false
-  started     = true
+  started     = false
 
   agent {
     enabled = true
@@ -40,7 +40,7 @@ resource "proxmox_virtual_environment_vm" "k3s01" {
   }
 
   cpu {
-    cores = 2
+    cores = 4
     type  = "host"
   }
 
@@ -57,7 +57,7 @@ resource "proxmox_virtual_environment_vm" "k3s01" {
   }
 
   memory {
-    dedicated = 4096
+    dedicated = 8192
   }
 
   initialization {
@@ -77,7 +77,77 @@ resource "proxmox_virtual_environment_vm" "k3s01" {
 
   network_device {
     bridge   = "vmbr0"
-    enabled  = true
     firewall = true
+  }
+
+  lifecycle {
+    ignore_changes = [disk]
+  }
+}
+
+module "proxmox_cloud_init_config_hermes01" {
+  source                                                  = "./modules/proxmox_cloud_init_config"
+  proxmox_cloud_init_config_vm_name                       = "hermes01"
+  proxmox_cloud_init_config_virtual_environment_node_name = "proxmox01"
+  proxmox_cloud_init_config_ssh_authorized_keys           = data.onepassword_item.ssh_key.public_key
+}
+
+resource "proxmox_virtual_environment_vm" "hermes01" {
+  name        = "hermes01"
+  vm_id       = 105
+  description = "Created with OpenTofu."
+  tags        = ["vm", "opentofu", "tailscale", "192.168.0.5"]
+  node_name   = "proxmox01"
+  on_boot     = false
+  started     = false
+
+  agent {
+    enabled = true
+    trim    = true
+  }
+
+  cpu {
+    cores = 2
+    type  = "host"
+  }
+
+  scsi_hardware = "virtio-scsi-single"
+
+  disk {
+    interface    = "scsi0"
+    datastore_id = "lv-ssd-crucial"
+    file_id      = proxmox_virtual_environment_download_file.almalinux_9_latest_cloud_image.id
+    size         = 50
+    discard      = "on"
+    ssd          = true
+    iothread     = true
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  initialization {
+    datastore_id = "lv-ssd-crucial"
+    ip_config {
+      ipv4 {
+        address = "192.168.0.5/24"
+        gateway = "192.168.0.1"
+      }
+    }
+    user_data_file_id = module.proxmox_cloud_init_config_hermes01.cloud_init_config_id
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  network_device {
+    bridge   = "vmbr0"
+    firewall = true
+  }
+
+  lifecycle {
+    ignore_changes = [disk]
   }
 }
